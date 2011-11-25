be39db4a9aa3fe9e35f95acc30e6990324a006fa
Andrew Halligan <ahalligan@nearlyfreehosting.com>
2011-11-25 08:00:09 -0500

    Port VOTG content to new template

--- voiceonthego/urls.py
+++ voiceonthego/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('voiceonthego.views',
     url(r'^$', 'index'),
+    url(r'^job$', 'job'),
+    url(r'^goals$', 'goals'),
+    url(r'^experience$', 'experience'),
+    url(r'^acknowledgments$', 'acknowledgments'),
 )
 
--- voiceonthego/views.py
+++ voiceonthego/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def index(request):
-    return render_to_response('content/index.html', {
+    return render_to_response('voiceonthego/index.html', {
         'organization_name': 'Voice On The Go',
         'organization_id': 'voiceonthego'      
-    })
+    }, context_instance = RequestContext(request))
+
+def job(request):
+    return render_to_response('voiceonthego/job.html', {
+        'organization_name': 'Voice On The Go',
+        'organization_id': 'voiceonthego'         
+    }, context_instance = RequestContext(request))
+
+def goals(request):
+    return render_to_response('voiceonthego/goals.html', {
+        'organization_name': 'Voice On The Go',
+        'organization_id': 'voiceonthego'         
+    }, context_instance = RequestContext(request))
+
+def experience(request):
+    return render_to_response('voiceonthego/experience.html', {
+        'organization_name': 'Voice On The Go',
+        'organization_id': 'voiceonthego'         
+    }, context_instance = RequestContext(request))
+
+def acknowledgments(request):
+    return render_to_response('voiceonthego/acknowledgments.html', {
+        'organization_name': 'Voice On The Go',
+        'organization_id': 'voiceonthego'         
+    }, context_instance = RequestContext(request))
 
